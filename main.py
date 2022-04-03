d2e9b8a29d36a710a0c2fd52759ed851afefceda
Alejandro <alejopadido13@gmail.com>
2022-04-02 23:32:50 -0500

    Version alfa terminada. 1.0

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import random
 import sys
+import os
+import keyboard
+
+
+# def kb():
+#     while True:
+#         if keyboard.is_pressed('enter'):
+#             print("A key was pressed")
+#             sys.exit(0)
+
+
+def clear():
+    if os.name == "posix":
+        os.system("clear")
+    elif os.name == ("ce", "nt", "dos"):
+        os.system("cls")
 
 
 def normalize(s):
@@ -22,51 +38,105 @@ def verify(x, y):
         return False
 
 
+def game():
+    try:
+        while True:
+            if keyboard.is_pressed('enter'):
+                clear()
+                with open('./data.txt') as f:
+                    words = [line.rstrip() for line in f]
+
+                n = random.randint(0, len(words))
+
+                selected = (words[n])
+                hidden = selected
+                enumerated_selected = dict(enumerate(selected, 1))
+
+                for i in range(1, len(enumerated_selected) + 1):
+                    x = enumerated_selected.get(i)
+                    hidden = hidden.replace(x, '_',)
+
+
+                coincidence = []
+                lifes = 6
+
+                while not verify(hidden, selected):
+                    clear()
+                    print('''
+            ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
+            ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
+            ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
+            ██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
+            ██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
+            ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+    
+    
+    
+    
+    
+    
+                        ''')
+                    print(hidden, '\n')
+                    print('Letras: ', len(selected))
+                    print('Intentos: ', lifes)
+                    user_attemp = input('\nLetra: ')
+
+                    if not (user_attemp in normalize(selected)):
+                        lifes -= 1
+
+                    for i in enumerated_selected:
+                        if normalize(enumerated_selected.get(i)) == user_attemp:
+                            coincidence.append(i)
+
+                    if lifes == 0:
+                        print('\nLa palabra era: ', selected)
+                        sys.exit()
+
+                    for i in coincidence:
+                        hidden = hidden[:i - 1] + enumerated_selected.get(i) + hidden[i:]
+                clear()
+                print('''
+        ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
+        ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
+        ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
+        ██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
+        ██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
+        ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+    
+    
+    
+                                    ''')
+                print(hidden, '\n')
+                print('Intentos: ', lifes)
+                print('Lo lograste!')
+                sys.exit()
+    except KeyboardInterrupt:
+        print('\n\nSee you later!')
+        sys.exit()
+
+
 def run():
-    with open('./data.txt') as f:
-        words = [line.rstrip() for line in f]
+    # Welcome message
+    print('''
+    
+██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
+██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
+███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
+██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
+██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+    ''')
+    print('By Alejandro Parrado Di Domenico')
+    print(
+        '_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶\n')
 
-    n = random.randint(0, len(words))
-
-    selected = (words[n])
-    hidden = selected
-    enumerated_selected = dict(enumerate(selected, 1))
-
-
-    print(selected)
-
-    for i in range(1, len(enumerated_selected) + 1):
-        x = enumerated_selected.get(i)
-        hidden = hidden.replace(x, '_')
-
-    print(hidden)
-
-    coincidence = []
-    lifes = 6
-    while not verify(hidden, selected):
-        user_attemp = input('Letra: ')
-
-        for i in enumerated_selected:
-            if normalize(enumerated_selected.get(i)) == user_attemp:
-                coincidence.append(i)
-
-        if not (user_attemp in normalize(selected)):
-            lifes -= 1
-            print('Esa letra no está')
-            print('Vidas: ', lifes)
-
-        if lifes == 0:
-            sys.exit()
-
-        for i in coincidence:
-            hidden = hidden[:i - 1] + enumerated_selected.get(i) + hidden[i:]
-
-        print(hidden)
-
-    print('Lo lograste!')
-
-
-
+    print('''Instructions:
+    
+        1. To start the game press Enter.
+        2. To exit the game press Esc.
+        3. Enjoy!
+    ''')
+    game()
 
 
 if __name__ == '__main__':
